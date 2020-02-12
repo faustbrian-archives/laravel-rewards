190d225d8428bc66b9d4514d0b552a0ee090ab5b
Brian Faust <hello@basecode.sh>
2020-02-12 15:45:43 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,17 @@
 <?php
 
-namespace Laravel\Cashier\Tests;
+declare(strict_types=1);
+
+/*
+ * This file is part of Skeleton.
+ *
+ * (c) KodeKeep <hello@kodekeep.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KodeKeep\Skeleton\Tests;
 
 use Illuminate\Contracts\Console\Application;
 use KodeKeep\Skeleton\Facades\SkeletonFacade;
@@ -9,12 +20,12 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    protected function getPackageProviders(Application $app): array
+    protected function getPackageProviders($app): array
     {
         return [SkeletonServiceProvider::class];
     }
 
-    protected function getPackageAliases(Application $app): array
+    protected function getPackageAliases($app): array
     {
         return ['Skeleton' => SkeletonFacade::class];
     }
